8db0bad05ce0403828e31d50229529fac2ba7bef
akshat-shah-2003 <akshats1607@gmail.com>
2025-09-13 10:45:34 +0530

    installed dependencies

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Akshat Shah",
+    author_email="akshats1607@gmail.com",
+    packages=find_packages()
+)
